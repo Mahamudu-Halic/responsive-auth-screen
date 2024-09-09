6020335e23c2f2557c01cc8ec91359639f8d2c65
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-09-09 20:40:15 +0000

    auth component slider added

---
+++ src/components/auth/auth-slider.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  AuthDescriptionContainer,
+  AuthSliderContainer,
+  ImageContainer,
+} from "../styled components/containers";
+import { ImageComponent } from "../styled components/image";
+import { H2 } from "../styled components/headings";
+import { Text } from "../styled components/content";
+
+type Props = {};
+
+const AuthSliderComponent = (props: Props) => {
+  return (
+    <AuthSliderContainer>
+      <ImageContainer width="300px" height="230px">
+        <ImageComponent src="/assets/social-girl.png" alt="social girl" />
+      </ImageContainer>
+
+      <AuthDescriptionContainer>
+        <H2 color="#263238">Exam Mastery Hub</H2>
+        <Text align="center" color="#181c19" size={0.9}>
+          Unleash Your Accademic Success with Exam Mastery Hub’s Exam Excellence
+          Platform
+        </Text>
+      </AuthDescriptionContainer>
+    </AuthSliderContainer>
+  );
+};
+
+export default AuthSliderComponent;
