826e6e76d5523ca70ef71b8ce3d082a21c1ce551
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-09-09 17:36:13 +0000

    auth page container components added

--- src/components/styled components/containers.ts
+++ src/components/styled components/containers.ts
@@ -1,9 +1,58 @@
 import styled from "styled-components";
 
+export const AuthPageContainer = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  background: #C1E0CA;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
+`;
 
 export const AuthContainer = styled.div`
-    display: flex;
-    height: 100vh;
-    width: 100%;
-    background: red;
+  display: flex;
+  height: 100%;
+  width: 100%;
+  background: green;
+  justify-content: center;
+  align-items: center;
+  border-radius: 15px;
+  box-shadow: 0px 0px 15px 5px rgba(0, 0, 0, 0.3);
+  overflow: hidden;
+`;
+
+export const AuthImageContainer = styled.div`
+position: relative;
+height: 230px;
+width: 300px;
+`;
+
+export const AuthFormContainer = styled.div`
+display: flex;
+height: 100%;
+width: 100%;
+background: #fff;
+flex: 1;
+`;
+
+export const AuthSliderContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+position: relative;
+height: 100%;
+flex: 1;
+background: #E6FEED;
+gap: 20px;
+
+`
+export const AuthDescriptionContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: 389px;
+gap: 15px;
 `
