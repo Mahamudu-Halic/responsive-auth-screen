e75a0a4f9acb64bac5eb1a082d48e93a391f47e0
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-09-09 17:37:25 +0000

    styled components added

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,19 +1,47 @@
-"use client"
+"use client";
 
-import { AuthContainer } from '@/components/styled components/containers'
-import React from 'react'
+import {
+  AuthContainer,
+  AuthDescriptionContainer,
+  AuthFormContainer,
+  AuthImageContainer,
+  AuthPageContainer,
+  AuthSliderContainer,
+} from "@/components/styled components/containers";
+import { Text } from "@/components/styled components/content";
+import { H2 } from "@/components/styled components/headings";
+import { ImageComponent } from "@/components/styled components/image";
+import Image from "next/image";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const SignInPage = (props: Props) => {
   return (
-    <div>
-        <AuthContainer>
-            img
-            form
-        </AuthContainer>
-    </div>
-  )
-}
+    <AuthPageContainer>
+      <AuthContainer>
+        {/* auth slider */}
+        <AuthSliderContainer>
+          <AuthImageContainer>
+            <ImageComponent src="/assets/social-girl.png" alt="social girl" />
+          </AuthImageContainer>
 
-export default SignInPage
+          <AuthDescriptionContainer>
+            <H2 color="#263238">Exam Mastery Hub</H2>
+            <Text align="center" color="#181c19">
+              Unleash Your Accademic Success with Exam Mastery Hub’s Exam
+              Excellence Platform
+            </Text>
+          </AuthDescriptionContainer>
+        </AuthSliderContainer>
+
+        {/* auth form */}
+        <AuthFormContainer>
+          <h1>form here</h1>
+        </AuthFormContainer>
+      </AuthContainer>
+    </AuthPageContainer>
+  );
+};
+
+export default SignInPage;
