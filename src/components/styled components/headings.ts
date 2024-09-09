57209bf8795fe53e35412b4aef504f3339f1aaba
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-09-09 20:51:11 +0000

    props added: align and size

--- src/components/styled components/headings.ts
+++ src/components/styled components/headings.ts
@@ -1,31 +1,43 @@
 import styled from "styled-components";
 
-export const H1 = styled.h1<{ color?: string; transform?: string }>`
+export const H1 = styled.h1<{ color?: string; transform?: string, align?: string; }>`
   text-transform: ${({ transform }) => transform || "inherit"};
   color: ${({ color }) => color || "inherit"};
+  text-align: ${({ align }) => align || "inherit"};
+  font-size: 3rem
 `;
 
-export const H2 = styled.h2<{ color?: string; transform?: string }>`
+export const H2 = styled.h2<{ color?: string; transform?: string, align?: string; }>`
   text-transform: ${({ transform }) => transform || "inherit"};
   color: ${({ color }) => color || "inherit"};
+  text-align: ${({ align }) => align || "inherit"};
+  font-size: 2rem
 `;
 
-export const H3 = styled.h3<{ color?: string; transform?: string }>`
+export const H3 = styled.h3<{ color?: string; transform?: string, align?: string; }>`
   text-transform: ${({ transform }) => transform || "inherit"};
   color: ${({ color }) => color || "inherit"};
+  text-align: ${({ align }) => align || "inherit"};
+  font-size: 2rem
 `;
 
-export const H4 = styled.h4<{ color?: string; transform?: string }>`
+export const H4 = styled.h4<{ color?: string; transform?: string, align?: string; }>`
   text-transform: ${({ transform }) => transform || "inherit"};
   color: ${({ color }) => color || "inherit"};
+  text-align: ${({ align }) => align || "inherit"};
+  font-size: 2rem
 `;
 
-export const H5 = styled.h5<{ color?: string; transform?: string }>`
+export const H5 = styled.h5<{ color?: string; transform?: string, align?: string; }>`
   text-transform: ${({ transform }) => transform || "inherit"};
   color: ${({ color }) => color || "inherit"};
+  text-align: ${({ align }) => align || "inherit"};
+  font-size: 2rem
 `;
 
-export const H6 = styled.h6<{ color?: string; transform?: string }>`
+export const H6 = styled.h6<{ color?: string; transform?: string, align?: string; }>`
   text-transform: ${({ transform }) => transform || "inherit"};
   color: ${({ color }) => color || "inherit"};
+  text-align: ${({ align }) => align || "inherit"};
+  font-size: 2rem
 `;
