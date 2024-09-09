94985c845abef848ab16c374b0969bcd5b25fca9
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-09-09 17:54:40 +0000

    text transformation added

--- src/components/styled components/headings.ts
+++ src/components/styled components/headings.ts
@@ -1,25 +1,31 @@
 import styled from "styled-components";
 
-export const H1 = styled.h1<{ color?: string }>`
+export const H1 = styled.h1<{ color?: string; transform?: string }>`
+  text-transform: ${({ transform }) => transform || "inherit"};
   color: ${({ color }) => color || "inherit"};
 `;
 
-export const H2 = styled.h2<{ color?: string }>`
+export const H2 = styled.h2<{ color?: string; transform?: string }>`
+  text-transform: ${({ transform }) => transform || "inherit"};
   color: ${({ color }) => color || "inherit"};
 `;
 
-export const H3 = styled.h3<{ color?: string }>`
+export const H3 = styled.h3<{ color?: string; transform?: string }>`
+  text-transform: ${({ transform }) => transform || "inherit"};
   color: ${({ color }) => color || "inherit"};
 `;
 
-export const H4 = styled.h4<{ color?: string }>`
+export const H4 = styled.h4<{ color?: string; transform?: string }>`
+  text-transform: ${({ transform }) => transform || "inherit"};
   color: ${({ color }) => color || "inherit"};
 `;
 
-export const H5 = styled.h5<{ color?: string }>`
+export const H5 = styled.h5<{ color?: string; transform?: string }>`
+  text-transform: ${({ transform }) => transform || "inherit"};
   color: ${({ color }) => color || "inherit"};
 `;
 
-export const H6 = styled.h6<{ color?: string }>`
+export const H6 = styled.h6<{ color?: string; transform?: string }>`
+  text-transform: ${({ transform }) => transform || "inherit"};
   color: ${({ color }) => color || "inherit"};
 `;
