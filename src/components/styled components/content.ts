0c88b588d6be544814f9a39b5aaf761f97aca31e
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-09-09 17:34:06 +0000

    content component for text and link added

---
+++ src/components/styled components/content.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+type Align = "center" | "left" | "right";
+
+export const Text = styled.p<{ align?: Align; color?: string; size?: number }>`
+  color: ${({color}) => color || "inherit"};
+  font-size: ${({size}) => size || 1}rem;
+  line-height: 24px;
+
+  ${({ align }) => {
+    switch (align) {
+      case "center":
+        return `
+            text-align: center;
+                `;
+      case "left":
+        return `
+            text-align: left;
+                `;
+      case "right":
+        return `
+            text-align: right;
+                `;
+      default:
+        return ``;
+    }
+  }}
+`;
+
+
+export const Link = styled.a``;
+
