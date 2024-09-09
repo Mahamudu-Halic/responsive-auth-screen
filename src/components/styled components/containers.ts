72f00f9690ec8b3159d7d130b82e36de960b732c
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-09-09 20:47:06 +0000

    added media query and OptionContainer component

--- src/components/styled components/containers.ts
+++ src/components/styled components/containers.ts
@@ -4,10 +4,18 @@ export const AuthPageContainer = styled.div`
   display: flex;
   height: 100vh;
   width: 100%;
-  background: #C1E0CA;
+  background: #c1e0ca;
   justify-content: center;
   align-items: center;
   padding: 50px;
+
+  @media (max-width: 820px) {
+    padding: 30px;
+  }
+  @media (max-width: 450px) {
+    padding: 10px;
+  }
+
 `;
 
 export const AuthContainer = styled.div`
@@ -22,39 +30,50 @@ export const AuthContainer = styled.div`
   overflow: hidden;
 `;
 
-export const AuthImageContainer = styled.div`
-position: relative;
-height: 230px;
-width: 300px;
+export const ImageContainer = styled.div<{ height: string; width: string }>`
+  position: relative;
+  height: ${({ height }) => height};
+  width: ${({ width }) => width};
 `;
 
 export const AuthFormContainer = styled.div`
-display: flex;
-height: 100%;
-width: 100%;
-background: #fff;
-justify-content: center;
-align-items: center;
-flex: 1;
+  display: flex;
+  height: 100%;
+  width: 100%;
+  background: #fff;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
 `;
 
 export const AuthSliderContainer = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-position: relative;
-height: 100%;
-flex: 1;
-background: #E6FEED;
-gap: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  height: 100%;
+  flex: 1;
+  background: #e6feed;
+  gap: 20px;
 
-`
+  @media (max-width: 720px) {
+    display: none;
+  }
+`;
 export const AuthDescriptionContainer = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-width: 389px;
-gap: 15px;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 319px;
+  gap: 15px;
+`;
+
+export const OptionContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  gap: 10px;
+`;
